c022dcd9252c4095d66acccf6fd56d158652e3de
panxo87sl <savage87.sl@gmail.com>
2025-03-03 02:49:57 -0300

    Configuracion github

--- js/Evento.js
+++ js/Evento.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 export default class Evento {
   constructor(id, imagen, nombre, productora, lugar, fecha, enlace) {
     this.id = id;
@@ -10,16 +9,3 @@ export default class Evento {
     this.enlace = enlace;
   }
 }
-=======
-export default class Evento {
-  constructor(id, imagen, nombre, productora, lugar, fecha, enlace) {
-    this.id = id;
-    this.imagen = imagen;
-    this.nombre = nombre;
-    this.productora = productora;
-    this.lugar = lugar;
-    this.fecha = fecha;
-    this.enlace = enlace;
-  }
-}
->>>>>>> 2b7948a29fbcf26e8b06dcb624ec22d706033dd8
